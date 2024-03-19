0d7cee23e027a69ca7fda9ed55d088ecb45d2f07
razvan999 <razvanciobanu999@gmail.com>
2024-03-19 18:59:25 +0200

    test

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,38 +1,24 @@
-const fs = require('fs');
-var json_file = "../data/graph_output.json";
-
 const express = require('express');
 const app = express();
+const path = require('path');
+const fs = require('fs');
 
 app.get('/', async (req, res) => {
-    try {
-      const jsonData = await get_json('your_json_file.json');
+  res.sendFile(path.join(__dirname, './html/view_ngraph.html'));
+});
 
-      res.status(200).json(jsonData);
-    } catch (error) {
-      console.error('Error:', error);
+app.get('/get_graph', (req, res) => {
+  fs.readFile('../data/graph_output.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error('Error reading the JSON file:', err);
       res.status(500).send('Internal Server Error');
+      return;
     }
+    
+    res.json(JSON.parse(data));
   });
+});
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
-
-function get_json() {
-    return new Promise((resolve, reject) => {
-      fs.readFile(json_file, 'utf8', (err, data) => {
-        if (err) {
-          reject('Error reading JSON file: ' + err);
-          return;
-        }
-        
-        try {
-          const jsonData = JSON.parse(data);
-          resolve(jsonData);
-        } catch (error) {
-          reject('Error parsing JSON data: ' + error);
-        }
-      });
-    });
-  }
